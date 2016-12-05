9ded7e19bc22a14dab58ef8b2dee32afe02afc92
Varun Ramesh <varunramesh@live.com>
2016-12-05 05:17:52 -0800

    Print out the ast of files.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,10 @@
 module Main where
 
-import Lib
+import System.Environment
+import Language.ExtLua.Parser
 
 main :: IO ()
-main = someFunc
+main = do
+  [file] <- getArgs
+  result <- parseFile file
+  print $ show result
